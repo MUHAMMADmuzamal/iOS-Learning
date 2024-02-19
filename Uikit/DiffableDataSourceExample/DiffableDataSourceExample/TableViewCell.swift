e1927658e698c460d2fce6477e92cd385b5856fb
Muhammad Muzamal <muzamal@codesorbit.com>
2024-02-19 13:18:04 +0500

    learn DiffableDataSource TableView.

---
+++ Uikit/DiffableDataSourceExample/DiffableDataSourceExample/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  DiffableDataSourceExample
+//
+//  Created by Codes Orbit on 19/02/2024.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
