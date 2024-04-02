e6b7dcc4db49a041a5eb886686d0ac58fe288cfb
Muhammad Muzamal <muzamal@codesorbit.com>
2024-04-02 10:56:17 +0500

    setup Algorithms Visualizer project.

---
+++ Projects/Algorithms Visualizer/Algorithms Visualizer/UI Layer/Algorithms_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Algorithms_VisualizerApp.swift
+//  Algorithms Visualizer
+//
+//  Created by Codes Orbit on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Algorithms_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
