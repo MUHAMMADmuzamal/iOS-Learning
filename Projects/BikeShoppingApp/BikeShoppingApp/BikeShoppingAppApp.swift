b4643fc7577da94158ac02f6bb8beebba0d779de
Muhammad Muzamal <muzamal@codesorbit.com>
2024-05-21 17:09:59 +0500

    create project

---
+++ Projects/BikeShoppingApp/BikeShoppingApp/BikeShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeShoppingAppApp.swift
+//  BikeShoppingApp
+//
+//  Created by Codes Orbit on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BikeShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
