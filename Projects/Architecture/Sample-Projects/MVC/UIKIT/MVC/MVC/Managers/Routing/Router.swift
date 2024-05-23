ee61f852172b85cccc86eea6b12608da5a2cb2ed
Muhammad Muzamal <muzamal@codesorbit.com>
2024-05-23 12:00:25 +0500

    add transitions files.

--- Projects/Architecture/Sample-Projects/MVC/UIKIT/MVC/MVC/Managers/Routing/Router.swift
+++ Projects/Architecture/Sample-Projects/MVC/UIKIT/MVC/MVC/Managers/Routing/Router.swift
@@ -5,4 +5,52 @@
 //  Created by Codes Orbit on 21/05/2024.
 //
 
-import Foundation
+import UIKit
+import Swinject
+
+protocol Closable: AnyObject {
+    func close()
+}
+
+protocol RouterProtocol: AnyObject {
+    
+    associatedtype U: UIViewController
+    
+    var viewController: U? { get }
+    
+    var injector: Container { get set }
+    
+    func open(_ viewController: UIViewController, transition: Transition)
+}
+
+class Router<U>: RouterProtocol, Closable, InjectorProtocol where U: UIViewController {
+    
+    var injector: Container
+    
+    init(injector: Container) {
+        self.injector = injector
+    }
+    
+    typealias U = U
+    
+    weak var viewController: U?
+    
+    var openTransition: Transition?
+
+    func open(_ viewController: UIViewController, transition: Transition) {
+        transition.viewController = self.viewController
+        transition.open(viewController)
+    }
+
+    func close() {
+        guard let openTransition = openTransition else {
+            assertionFailure("You should specify an open transition in order to close a module.")
+            return
+        }
+        guard let viewController = viewController else {
+            assertionFailure("Nothing to close.")
+            return
+        }
+        openTransition.close(viewController)
+    }
+}
