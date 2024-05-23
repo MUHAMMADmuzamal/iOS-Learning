17ffe8859fb626fdbb6bae727ec130ec4dd08bae
Muhammad Muzamal <muzamal@codesorbit.com>
2024-05-23 11:24:10 +0500

    add extension file and setup pods.

---
+++ Projects/Architecture/Sample-Projects/MVC/UIKIT/MVC/MVC/Extensions/Combine/Publisher-Extension.swift
@@ -0,0 +1,29 @@
+//
+//  Publisher-Extension.swift
+//  MVC
+//
+//  Created by Codes Orbit on 23/05/2024.
+//
+
+import Foundation
+import Combine
+
+extension Publisher {
+    static func empty() -> AnyPublisher<Output, Failure> {
+        return Empty().eraseToAnyPublisher()
+    }
+
+    static func just(_ output: Output) -> AnyPublisher<Output, Failure> {
+        return Just(output)
+            .setFailureType(to: Failure.self)
+            .eraseToAnyPublisher()
+    }
+
+    static func fail(_ error: Failure) -> AnyPublisher<Output, Failure> {
+        return Fail(error: error).eraseToAnyPublisher()
+    }
+    
+    func flatMapLatest<T: Publisher>(_ transform: @escaping (Self.Output) -> T) -> Publishers.SwitchToLatest<T, Publishers.Map<Self, T>> where T.Failure == Self.Failure {
+          map(transform).switchToLatest()
+      }
+}
