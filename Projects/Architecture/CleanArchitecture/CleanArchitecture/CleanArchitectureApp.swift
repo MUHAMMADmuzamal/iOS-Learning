f817a2adb55f73db3bb7884642c619586fcc24ab
Muhammad Muzamal <muzamal@codesorbit.com>
2024-06-11 22:40:31 +0500

    setup project

---
+++ Projects/Architecture/CleanArchitecture/CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Codes Orbit on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
