527f1e27b6885c572d9c2b68b3d4ca8fd1967b17
Muhammad Muzamal <muzamal@codesorbit.com>
2024-06-13 15:48:01 +0500

    setup project + add onboarding moudule.

---
+++ Projects/Tridoy/CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Codes Orbit on 11/06/2024.
+//
+
+import SwiftUI
+import Onboarding
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ Projects/Tridoy/Modules/Onboarding/Onboarding/Presentation/SplashView.swift
@@ -0,0 +1,21 @@
+//
+//  SplashView.swift
+//  Onboarding
+//
+//  Created by Codes Orbit on 13/06/2024.
+//
+
+import SwiftUI
+
+public struct SplashView: View {
+    public init(){}
+    public var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
