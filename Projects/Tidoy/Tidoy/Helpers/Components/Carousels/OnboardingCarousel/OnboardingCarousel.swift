4f1023ee6955015d78efb713551833dbfc061a07
Muhammad Muzamal <muzamal@codesorbit.com>
2024-07-13 18:46:06 +0500

    make onboarding carousel

--- Projects/Tidoy/Tidoy/Helpers/Components/Carousels/OnboardingCarousel/OnboardingCarousel.swift
+++ Projects/Tidoy/Tidoy/Helpers/Components/Carousels/OnboardingCarousel/OnboardingCarousel.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct OnboardingCarousel: View {
+    private let dataSource = [
+        OnboardingCarouselCardModel(
+           image: "onboardingCarousel-Image1",
+           title: "Gateway to Your Adventure",
+           subTitle: "Enjoy various housing options, from budget to luxury, in Tidoy."), 
+        OnboardingCarouselCardModel(
+           image: "onboardingCarousel-Image2",
+           title: "Discover the Wonders of the World: Let's Explore!",
+           subTitle: "Book a stay wherever you are, whenever you want."),
+        OnboardingCarouselCardModel(
+           image: "onboardingCarousel-Image3",
+           title: "The Right Solution for Your Holiday Accommodation",
+           subTitle: "A stress-free Holiday? Trust your Holiday accommodation to Tidoy!")
+    ]
+    let heightOfIndicator: CGFloat = 8.0
+    let spacingIndicator: CGFloat = 8.0
+    let widthOfSelectedIndicator: CGFloat = 26.0
+    @State private var index: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TabView(selection: $index) {
+                ForEach(0..<dataSource.count, id: \.self) { index in
+                    OnboardingCarouselCard(model: dataSource[index]).tag(index)
+                }
+            }.tabViewStyle(.page(indexDisplayMode: .never))
+            
+            HStack(spacing: spacingIndicator) {
+                ForEach(0..<dataSource.count, id: \.self) { index in
+                    if self.index == index {
+                        RoundedRectangle(cornerRadius: heightOfIndicator)
+                            .fill(.backgroundPrimary)
+                            .frame(width: widthOfSelectedIndicator, height: heightOfIndicator)
+                    } else {
+                        Circle()
+                            .fill(.background30)
+                            .frame(width: heightOfIndicator)
+                    }
+                }
+            }
+        }
     }
 }
 
--- Projects/Tidoy/Tidoy/Helpers/Components/Carousels/OnboardingCarousel/OnboardingCarouselCard.swift
+++ Projects/Tidoy/Tidoy/Helpers/Components/Carousels/OnboardingCarousel/OnboardingCarouselCard.swift
@@ -14,26 +14,47 @@ struct OnboardingCarouselCardModel {
 }
 
 struct OnboardingCarouselCard: View {
-    @Binding var  model: OnboardingCarouselCardModel
+    var  model: OnboardingCarouselCardModel
     var body: some View {
         VStack {
             Image(model.image)
+                .resizable()
+                .scaledToFit()
+                .overlay {
+                    VStack {
+                        LinearGradient(stops: [
+                            .init(color: .clear, location: 0.53),
+                            .init(color: .white, location: 1)],
+                                       startPoint: .bottom,
+                                       endPoint: .top)
+                        
+                        LinearGradient(stops: [
+                            .init(color: .clear, location: 0.53),
+                                .init(color: .white, location: 1)],
+                                       startPoint: .top,
+                                       endPoint: .bottom)
+                    }
+                }
+                .clipShape(RoundedRectangle(cornerRadius: .cornerRadiusM))
+                
             VStack(alignment: .leading) {
                 Text(model.title)
                     .font(.heading4)
+                    .foregroundStyle(.text100)
                 Text(model.subTitle)
                     .font(.bodyMediumRegular)
+                    .foregroundStyle(.text60)
             }
-            .padding(.horizontal, .padding16)
         }
+        .padding(.horizontal, .padding16)
     }
 }
 
 #Preview {
      let model = OnboardingCarouselCardModel(
-        image: "onboardingCarousel-Image1",
+        image: "onboardingCarousel-Image3",
         title: "Gateway to Your Adventure",
         subTitle: "Enjoy various housing options, from budget to luxury, in Tidoy.")
     
-    return OnboardingCarouselCard(model: .constant(model))
+    return OnboardingCarouselCard(model: model)
 }
