0ee3d6c6e7fa353a527377d4a74eec7ce6b996fa
Muhammad Muzamal <muzamal@codesorbit.com>
2024-08-20 14:20:35 +0500

    Add Phone Number field and Country list view.

---
+++ Projects/Tidoy/Tidoy/Helpers/Components/CountryListView/CountryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountryListView.swift
+//  Tidoy
+//
+//  Created by Codes Orbit on 20/08/2024.
+//
+
+import SwiftUI
+
+struct CountryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryListView()
+}
---
+++ Projects/Tidoy/Tidoy/Helpers/Components/TextField/PhoneNumberTextField.swift
@@ -0,0 +1,98 @@
+//
+//  PhoneNumberTextField.swift
+//  Tidoy
+//
+//  Created by Codes Orbit on 14/08/2024.
+//
+
+import SwiftUI
+
+struct PhoneNumberTextField: View {
+    @ObservedObject private var viewModel: PhoneNumberTextFieldViewModel
+    
+    var label: String
+    var hintText: String
+    var placeholderText: String
+    var action: (() -> Void)?
+    
+    init(fieldType: TextFieldTypeProtocol,
+         label: String,
+         hintText: String,
+         placeholderText: String,
+         action: (() -> Void)? = nil) {
+        
+        self.viewModel = PhoneNumberTextFieldViewModel(fieldType: fieldType)
+        self.label = label
+        self.hintText = hintText
+        self.placeholderText = placeholderText
+        self.action = action
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .foregroundColor(viewModel.state.labelColor)
+            HStack(spacing: .padding4) {
+                HStack {
+                    Text(viewModel.selectedCountry.flag)
+                    Text(viewModel.selectedCountry.code)
+                    Image(systemName: viewModel.isTapOnIcon ? "chevron.up" : "chevron.down")
+                }
+                .onTapGesture {
+                    viewModel.isTapOnIcon.toggle()
+                    self.action?()
+                }
+                TextField("", text: $viewModel.text)
+                    .onChange(of: viewModel.text) {
+                        viewModel.validate()
+                    }
+                    .onTapGesture {
+                        viewModel.onFocusChange(isFocused: true)
+                    }
+                    .onHover { isHovering in
+                        viewModel.onHover(isHovering: isHovering)
+                    }
+                    .foregroundColor(viewModel.state.textColor)
+                    .background {
+                        if viewModel.text.isEmpty {
+                             HStack {
+                                 Text(placeholderText)
+                                 Spacer()
+                             }
+                         }
+                    }
+            }
+            .foregroundStyle(.icon60)
+                .padding(.all, .padding12)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(viewModel.state.borderColor, lineWidth: 1.0)
+                }
+            Text(hintText)
+                .font(.bodyXSmallRegular)
+                .foregroundColor(viewModel.state.labelColor)
+        }
+        .padding(.horizontal, 2)
+        .disabled(viewModel.state == .disable)
+    }
+    
+    func setState(_ newState: StateOfTextField) -> some View {
+        self.viewModel.state = newState
+        return self
+    }
+}
+
+#Preview {
+    return VStack {
+        PhoneNumberTextField(fieldType: UserNameTextField(),
+                             label: "Phone Number",
+                             hintText: "Enter your username",
+                             placeholderText: "ex: 3465944619")
+        PhoneNumberTextField(fieldType: UserNameTextField(),
+                             label: "Phone Number",
+                             hintText: "Enter your username",
+                             placeholderText: "ex: +923465944619")
+        .setState(.error)
+    }
+
+}
