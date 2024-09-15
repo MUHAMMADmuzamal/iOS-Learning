d71252cb5a628818e308ea050ffa04c73fc8009e
Muhammad Muzamal <muzamal@codesorbit.com>
2024-09-15 18:41:03 +0500

    Save images to local storage.

--- Projects/Crypto/Crypto/Services/CoinImageService.swift
+++ Projects/Crypto/Crypto/Services/CoinImageService.swift
@@ -14,22 +14,36 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManger = LocalFileManager.instance
+    private let folderName: String = "coin_Images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
-        getCoinImage(url: coin.image)
+        self.imageName = coin.id
+        getCoinImage()
     }
     
-    private func getCoinImage(url: String) {
-        guard let url = URL(string: url) else { return }
+    private func getCoinImage() {
+        if let savedImage =  fileManger.getImage(imageName: imageName, folderName: folderName) {
+            self.image = savedImage
+        }else {
+            self.downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
+        guard let url = URL(string: self.coin.image) else { return }
         
         imageSubscription =  NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnImage) in
-                self?.image = returnImage
-                self?.imageSubscription?.cancel()
+                guard let self =  self, let downloadImage = returnImage else { return }
+                self.image = downloadImage
+                self.imageSubscription?.cancel()
+                self.fileManger.saveImage(image: downloadImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
