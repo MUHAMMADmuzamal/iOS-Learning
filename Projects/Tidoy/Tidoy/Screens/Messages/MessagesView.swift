646081b162e5f1ba52efc1bf1d1fc4bd6c95e0f0
MuhammadMuzamal-iOS <muhammad.muzamil@myday.health>
2025-05-23 10:21:38 +0500

    remove webSocket view code.

--- Projects/Tidoy/Tidoy/Screens/Messages/MessagesView.swift
+++ Projects/Tidoy/Tidoy/Screens/Messages/MessagesView.swift
@@ -8,78 +8,10 @@
 import SwiftUI
 
 struct MessagesView: View {
-    @StateObject private var webSocketManager = MessagesVM()
-    @State private var messageToSend = ""
-    @Environment(\.dismiss) private var dismiss
-    @State private var isPresentError: Bool = true
+
     
     var body: some View {
-        VStack {
-            Text(webSocketManager.isConnected ? "Connected ✅" : "Disconnected ❌")
-                .foregroundColor(webSocketManager.isConnected ? .green : .red)
-            
-            Text("Received: \(webSocketManager.receivedMessage)")
-                .padding()
-            
-            TextField("Enter message", text: $messageToSend)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            HStack {
-                Button("Connect") {
-                    webSocketManager.connect()
-                }
-                .buttonStyle(.bordered)
-                
-                Button("Send") {
-                    webSocketManager.sendMessage(messageToSend)
-                }
-                .buttonStyle(.bordered)
-                .disabled(messageToSend.isEmpty)
-                
-                Button("Disconnect") {
-                    webSocketManager.disconnect()
-                }
-                .buttonStyle(.bordered)
-            }
-        }
-        .modelAlert(isPresented: $isPresentError, model: CustomAlertModel(
-            title: "Error",
-            message: "Some thing went wrong!",
-            primaryButtonTitle: "OK",
-            primaryAction: { print("OK Pressed") },
-            secondaryButtonTitle: "Cancel",
-            secondaryAction: { print("Cancel Pressed")})
-        )
-        .padding()
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 25, style: .continuous)
-                        .fill(Color.gray)
-                        .frame(height: 50)  // Adjust height of nav bar
-                }
-            }
-            
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    dismiss() // Custom back action
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.left") // Custom icon
-                        Text("Back")
-                    }
-                    .foregroundStyle(Color.red)
-                }
-            }
-            
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {}) {
-                    Image(systemName: "gear")
-                }
-            }
-        }
+        
     }
 }
 
