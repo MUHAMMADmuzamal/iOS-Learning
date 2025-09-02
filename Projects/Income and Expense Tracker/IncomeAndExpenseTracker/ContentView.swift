986025cc4f443d276734ad82b76d29e0fec74cb5
MuhammadMuzamal-iOS <muhammad.muzamil@myday.health>
2025-09-02 23:04:10 +0500

    testing coordinator.

--- Projects/Income and Expense Tracker/IncomeAndExpenseTracker/ContentView.swift
+++ Projects/Income and Expense Tracker/IncomeAndExpenseTracker/ContentView.swift
@@ -7,15 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+enum Tabs: Int, CaseIterable {
+    case home, settings
+}
+
+class TapCoordinates: ObservableObject {
+    @Published var path: [String] = []
+    
+    func push(_ navigateTo: [String]) {
+        path.append(contentsOf: navigateTo)
+    }
+}
+
+
+class AppCoordinator: ObservableObject {
+ 
+    
+    @Published var selectedTab: Tabs = .home
+    var tabCoordinators: [TapCoordinates] = []
+    
+    
+    init() {
+        for _ in Tabs.allCases {
+            tabCoordinators.append(TapCoordinates())
         }
-        .padding()
+    }
+    
+    func selectTab(_ tab: Tabs) {
+        self.selectedTab = tab
+    }
+    
+    func selectTab(_ tab: Tabs, navigateTo: [String]) {
+        self.selectTab(tab)
+        tabCoordinators[tab.rawValue].push(navigateTo)
+    }
+}
+
+struct ContentView: View {
+    @StateObject var appCoordinator = AppCoordinator()
+    
+    var body: some View {
+        
+        TabView(selection: $appCoordinator.selectedTab) {
+            Tab("Home", systemImage: "tray.and.arrow.down.fill", value: .home) {
+                NavigationStack(path: $appCoordinator.tabCoordinators[Tabs.home.rawValue].path) {
+                    VStack {
+                        Text("Home")
+                        Button("Go to Settings/1/2/3") {
+                            appCoordinator.selectTab(.settings, navigateTo: ["1", "2","3"])
+                        }
+                    }
+                    .navigationDestination(for: String.self) { name in
+                        Text(name)
+                    }
+                }
+            }
+
+            
+            Tab("Setting", systemImage: "tray.and.arrow.up.fill", value: .settings) {
+                NavigationStack(path: $appCoordinator.tabCoordinators[Tabs.settings.rawValue].path) {
+                   
+                    VStack {
+                        Text("Setting")
+                        Button("Go to Home/Test") {
+                            appCoordinator.selectTab(.home, navigateTo: ["Test"])
+                        }
+                    }
+                        .navigationDestination(for: String.self) { name in
+                            Text(name)
+                        }
+                }
+            }
+
+        }
     }
 }
 
--- Projects/Income and Expense Tracker/IncomeAndExpenseTracker/Home.swift
+++ Projects/Income and Expense Tracker/IncomeAndExpenseTracker/Home.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 
 struct Home: View {
-    @StateObject var coordinator = HomeCoordinator()
+//    @StateObject var coordinator = HomeCoordinator()
     var body: some View {
-        NavigationStack(path: $coordinator.path) {
-            coordinator.build(page: .home)
-            .navigationTitle("Home")
-            .navigationDestination(for: HomePage.self) { page in
-                coordinator.build(page: page)
-            }
-            .sheet(item: $coordinator.sheet) { sheet in
-                coordinator.build(sheet: sheet)
-            }
-            .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreen in
-                coordinator.build(fullScreen: fullScreen)
-            }
-        }
+        ContentView()
+//        NavigationStack(path: $coordinator.path) {
+//            coordinator.build(page: .home)
+//            .navigationTitle("Home")
+//            .navigationDestination(for: HomePage.self) { page in
+//                coordinator.build(page: page)
+//            }
+//            .sheet(item: $coordinator.sheet) { sheet in
+//                coordinator.build(sheet: sheet)
+//            }
+//            .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreen in
+//                coordinator.build(fullScreen: fullScreen)
+//            }
+//        }
     }
 }
 
